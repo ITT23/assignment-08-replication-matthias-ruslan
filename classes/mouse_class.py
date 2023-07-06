fb2572c84aa4cd8e353804297800b890c4a9a53f
Ruslan Asabidi <ruslanasabidi@gmail.com>
2023-07-06 16:42:42 +0200

    mouse prototype
    mouse movement via accelerometer

--- classes/mouse_class.py
+++ classes/mouse_class.py
@@ -1,5 +1,8 @@
 # mouse class
 
+import pyautogui
+from Config import Config
+
 class Mouse():
 
     def __init__(self):
@@ -11,6 +14,9 @@ class Mouse():
     def set_mouse_pos(self, new_x_pos, new_y_pos):
         self.pos_x = new_x_pos
         self.pos_y = new_y_pos
+
+    def move(self):
+        pyautogui.moveRel(self.pos_x * Config.MOUSE_MOVEMENT_SCALING, self.pos_y * Config.MOUSE_MOVEMENT_SCALING) # scaled by 10
 
     def set_mouse_btn_1_state(self, new_left_btn_state):
         self.left_click = new_left_btn_state
--- mouse_controller.py
+++ mouse_controller.py
@@ -1,6 +1,8 @@
-from sender.DIPPID import SensorUDP
+import keyboard
+
+from DIPPID import SensorUDP
 from classes.mouse_class import Mouse
-from config.Config import Config
+from Config import Config
 
 # use UPD (via WiFi) for communication
 PORT = 5700
@@ -11,24 +13,33 @@ mouse_controller = Mouse()
 def handle_accelerometer_data(data):
     if(sensor.has_capability('accelerometer')):
         mouse_controller.set_mouse_pos(data['x'], data['y'])
+        mouse_controller.move()
     else:
         raise Exception(Config.MISSING_ACCELEROMETER_EXCEPTION)
     
 def handle_btn_1_data(data):
     if(sensor.has_capability('button_1')):
-        #mouse_controller.set_mouse_btn_1_state(data)
-        print(data)
+        mouse_controller.set_mouse_btn_1_state(data)
+        # for testing purposes
+        if mouse_controller.left_click == 1:
+            print("left mouse button was clicked")
     else:
         raise Exception(Config.MISSING_BTN_1_EXCEPTION)
 
 def handle_btn_2_data(data):
     if(sensor.has_capability('button_2')):
-        #mouse_controller.set_mouse_btn_1_state(data)
-        print(data)
+        mouse_controller.set_mouse_btn_2_state(data)
+        if mouse_controller.right_click == 1:
+            print("right mouse button was clicked")
     else:
-        raise Exception(Config.MISSING_BTN_1_EXCEPTION)
+        raise Exception(Config.MISSING_BTN_2_EXCEPTION)
 
 
 sensor.register_callback('accelerometer', handle_accelerometer_data)
 sensor.register_callback('button_1', handle_btn_1_data)
 sensor.register_callback('button_2', handle_btn_2_data)
+
+while(True):
+    if keyboard.is_pressed('q'):
+        sensor.disconnect()
+        break
